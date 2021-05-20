00ece88a9655672b03db92a794037d3bc300f30f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-20 10:09:53 +0300

    copy

---
+++ MCAT/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 245, green: 249, blue: 250)
+    
+    static let mainColor = UIColor(integralRed: 1, green: 143, blue: 161)
+    static let mainColorAlpha = UIColor(integralRed: 1, green: 143, blue: 161, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 250, green: 147, blue: 142)
+    
+    static let progress1Color = UIColor(integralRed: 1, green: 143, blue: 161)
+    static let progress2Color = UIColor(integralRed: 250, green: 147, blue: 142)
+    static let progress3Color = UIColor(integralRed: 1, green: 206, blue: 211)
+}
