6fd836ee830520027e9d53b64ba3b1d21a2a0765
akonst17 <akonst17@gmail.com>
2022-04-15 11:20:32 +0300

    upgrade

--- MCAT/Classes/Service/Stats/Share/StatsShareManager.swift
+++ MCAT/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.mcat.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.mcat.widget")
     }
     
     @discardableResult
