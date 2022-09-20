63c841ff58acb698b598faf37f4ac117a34ec9cb
akonst17 <akonst17@gmail.com>
2022-09-20 14:59:04 +0300

    интеграция sdk 2.0; новые урлы; build 17

--- MCAT/Classes/Misc/GlobalDefinitions.swift
+++ MCAT/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "fy2wuEZSiOP6xeoZ"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "pTD5SAPWu8y7C9z7"
-
     static let applicationTag = "EXAM_MCAT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "pTD5SAPWu8y7C9z7"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "dNTkYobPRRYsAyBI"
 
     static let appStoreUrl = "https://apps.apple.com/app/mcat-prep/id1568228652"
     static let appleAppID = "1568228652"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
